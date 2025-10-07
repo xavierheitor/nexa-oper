adbadb3336160c145ee79ac03372458e8f5bc265
Heitor Xavier <xavierheitor@hotmail.com>
2025-10-07 14:35:51 -0300

    escala nao atendeu, comecaremos novamente

--- apps/api/src/modules/escala/escala.module.ts
+++ apps/api/src/modules/escala/escala.module.ts
@@ -4,11 +4,12 @@
 
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@database/database.module';
+import { AuthModule } from '@modules/engine/auth/auth.module';
 import { EscalaController } from './controllers';
 import { EscalaService } from './services/escala.service';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AuthModule],
   controllers: [EscalaController],
   providers: [EscalaService],
   exports: [EscalaService],
