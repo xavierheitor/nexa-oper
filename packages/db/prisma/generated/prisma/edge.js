54f27af34c8c2dcd9814b9be780ec3a2edf7507e
Heitor Xavier <xavierheitor@hotmail.com>
2025-10-15 10:20:15 -0300

    correcao de erros

--- packages/db/prisma/generated/prisma/edge.js
+++ packages/db/prisma/generated/prisma/edge.js
@@ -1250,7 +1250,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../.env",
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../models",
--- packages/db/prisma/generated/prisma/index.js
+++ packages/db/prisma/generated/prisma/index.js
@@ -1251,7 +1251,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../.env",
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../models",
