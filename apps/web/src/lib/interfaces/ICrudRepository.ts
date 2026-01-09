b7995fb48b0cf73790796412e14d1b63cc2222dc
Heitor Xavier <xavierheitor@hotmail.com>
2026-01-08 21:38:55 -0300

    remoção de conversoees de tipo erradas

--- apps/web/src/lib/interfaces/ICrudRepository.ts
+++ apps/web/src/lib/interfaces/ICrudRepository.ts
@@ -1,8 +1,11 @@
 // src/lib/repositories/ICrudRepository.ts
-export interface ICrudRepository<T, F> {
-  create(data: T): Promise<T>;
-  update(id: string | number, data: Partial<T>): Promise<T>;
+export interface IBaseCrudRepository<T, F> {
   delete(id: string | number, userId: string): Promise<T>;
   findById(id: string | number): Promise<T | null>;
   list(params: F): Promise<{ items: T[]; total: number }>;
+}
+
+export interface ICrudRepository<T, F> extends IBaseCrudRepository<T, F> {
+  create(data: T): Promise<T>;
+  update(id: string | number, data: Partial<T>): Promise<T>;
 }
